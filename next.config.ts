555c561232c4eb2efbcd79a5046723fba382dea5
Vijay Jv <vjv7273@gmail.com>
2025-12-17 06:47:25 +0000

    fixed

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https,
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
